f14b3a997dbaf465f663a94d62e88d5fbcefecdb
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-02-02 10:08:35 +0530

    stop word removal

--- StopWordRemoval.py
+++ StopWordRemoval.py
@@ -1,23 +1,21 @@
 from pyspark.sql import SparkSession
 
-def wordcount_mapper(word):
+def mapper(word):
     if word not in ['the','that','The','a','is','these']:
         return (word)
 
 def splitter(line):
     return line.split(" ")
 
-spark =SparkSession.builder.appName("WordCount").master("local").getOrCreate()
+spark = SparkSession.builder.appName("WordCount").master("local").getOrCreate()
 
-read_file=spark.sparkContext.textFile("Input.txt")
+read_file = spark.sparkContext.textFile("Input.txt")
 
-words=read_file.flatMap(splitter)
+words = read_file.flatMap(splitter)
 
-wordcount_mapped=words.map(wordcount_mapper)
+ans = words.map(mapper).collect()
 
-temp = wordcount_mapped.collect()
-
-for x in temp:
+for x in ans:
     print(x)
 
 spark.stop()    
